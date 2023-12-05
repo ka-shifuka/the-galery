78c5109f92204592706479de49ad00cbf63fde64
ka-shifuka <aryadewanggapuja@gmail.com>
2023-12-06 06:41:16 +0700

    Add images and component images

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <!-- script -->
-<script setup></script>
+<script setup>
+  import Images from "./components/Images.vue";
+</script>
 <!-- end script -->
 
 <!-- template -->
 <template>
   <section class="@Container min-h-screen flex flex-col justify-between">
     <!-- header and main content -->
-    <div class="@wrap">header main</div>
+    <div class="@wrap p-6 flex flex-col gap-6">
+      header main
+      <Images url="/pictures/images_1.jpeg"></Images>
+      <Images url="/pictures/images_2.jpeg"></Images>
+      <Images url="/pictures/images_3.jpeg"></Images>
+      <Images url="/pictures/images_4.jpeg"></Images>
+      <Images url="/pictures/images_5.jpeg"></Images>
+      <Images url="/pictures/images_6.jpeg"></Images>
+      <Images url="/pictures/images_7.jpeg"></Images>
+      <Images url="/pictures/images_8.jpeg"></Images>
+    </div>
     <!-- endheader and main content -->
 
     <!-- footer -->
---
+++ src/components/Images.vue
@@ -0,0 +1,31 @@
+<!-- script -->
+<script setup>
+  const props = defineProps({
+    url: String
+  });
+</script>
+<!-- end script -->
+
+<!-- template -->
+<template>
+  <div class="@wrap w-fit h-fit relative">
+    <img class="w-full h-fit rounded-xl" :src="props.url" alt="pictures" />
+    <img
+      class="w-full h-fit rounded-xl blur-md absolute top-4 right-0 -z-10"
+      :src="props.url"
+      alt="pictures"
+    />
+    <a
+      class="absolute bottom-1 right-1 p-2 bg-[rgba(0,0,0,0)] backdrop-blur-2xl text-[12px] text-zinc-100 rounded-md"
+      download
+      href="props.url"
+    >
+      <i class="bi bi-arrow-down"></i> Download
+    </a>
+  </div>
+</template>
+<!-- end template -->
+
+<!-- style -->
+<style scoped></style>
+<!-- end stayle -->

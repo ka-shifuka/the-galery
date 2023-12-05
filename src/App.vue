cc07388cf2039b821056fc9210bbdf31fc4e0253
ka-shifuka <aryadewanggapuja@gmail.com>
2023-12-05 21:39:39 +0700

    Frist Configuration

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- script -->
+<script setup></script>
+<!-- end script -->
+
+<!-- template -->
+<template>
+  <section class="@Container min-h-screen flex flex-col justify-between">
+    <!-- header and main content -->
+    <div class="@wrap">header main</div>
+    <!-- endheader and main content -->
+
+    <!-- footer -->
+    <div class="@wrap">footer</div>
+    <!-- end footer -->
+  </section>
+</template>
+<!-- end template -->
+
+<!-- style -->
+<style scoped></style>
+<!-- end stayle -->

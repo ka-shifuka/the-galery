48c1136be9805b4673c452d0bfa3d20d64f12ab7
ka-shifuka <aryadewanggapuja@gmail.com>
2023-12-06 21:06:26 +0700

    Create a figcapton (slide view) in Images component

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,40 @@
 
 <!-- template -->
 <template>
+  <!-- height ground -->
+  <div
+    class="w-[200vh] -top-20 -left-20 h-20 absolute bg-black blur-xl -z-50"
+  ></div>
+  <!-- end height ground -->
+
   <section class="@Container min-h-screen flex flex-col justify-between">
     <!-- header and main content -->
     <div class="@wrap p-6 flex flex-col gap-6">
       header main
-      <Images url="/pictures/images_1.jpeg"></Images>
-      <Images url="/pictures/images_2.jpeg"></Images>
-      <Images url="/pictures/images_3.jpeg"></Images>
-      <Images url="/pictures/images_4.jpeg"></Images>
-      <Images url="/pictures/images_5.jpeg"></Images>
-      <Images url="/pictures/images_6.jpeg"></Images>
-      <Images url="/pictures/images_7.jpeg"></Images>
-      <Images url="/pictures/images_8.jpeg"></Images>
+      <Images
+        textwhite
+        title="Ryo yamada"
+        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
+        url="/pictures/images_1.jpeg"
+      ></Images>
+      <Images
+        textwhite
+        title="Ryo yamada"
+        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
+        url="/pictures/images_2.jpeg"
+      ></Images>
+      <Images
+        textwhite
+        title="Ryo yamada"
+        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
+        url="/pictures/images_3.jpeg"
+      ></Images>
+      <Images
+        textwhite
+        title="Ryo yamada"
+        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
+        url="/pictures/images_4.jpeg"
+      ></Images>
     </div>
     <!-- endheader and main content -->
 
--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,7 +1,29 @@
 <!-- script -->
 <script setup>
+  import { ref, onMounted } from "vue";
+
+  const slideActive = ref(false); // slide is to show figcaption
+  /*
+   * Define properties in this component
+   * url : url of images
+   * title : figure title
+   * description : figure description
+   * textwhite : use if the background is dark
+   */
   const props = defineProps({
-    url: String
+    url: String,
+    title: String,
+    description: String,
+    textwhite: Boolean
+  });
+  
+  /*
+   * if is scroll slideActive become false
+  */
+  onMounted(() => {
+    window.addEventListener("scroll", function () {
+      slideActive.value = false;
+    });
   });
 </script>
 <!-- end script -->
@@ -9,19 +31,59 @@
 <!-- template -->
 <template>
   <div class="@wrap w-fit h-fit relative">
-    <img class="w-full h-fit rounded-xl" :src="props.url" alt="pictures" />
-    <img
-      class="w-full h-fit rounded-xl blur-md absolute top-4 right-0 -z-10"
-      :src="props.url"
-      alt="pictures"
-    />
-    <a
-      class="absolute bottom-1 right-1 p-2 bg-[rgba(0,0,0,0)] backdrop-blur-2xl text-[12px] text-zinc-100 rounded-md"
-      download
-      href="props.url"
+    <figure>
+      <!-- main image -->
+      <img class="w-full h-fit rounded-xl" :src="props.url" alt="pictures" />
+      <!-- end  main image -->
+
+      <!-- background to create blur effect -->
+      <img
+        class="w-full h-fit rounded-xl blur-md absolute top-4 right-0 -z-10"
+        :src="props.url"
+        alt="pictures"
+      />
+      <!-- end background to create blur effect -->
+
+      <!-- download link -->
+      <a
+        class="absolute bottom-1.5 right-1.5 p-2 bg-transparent backdrop-blur-2xl text-[12px] text-zinc-100 rounded-md"
+        :href="props.url"
+        download
+      >
+        <i class="bi bi-arrow-down"></i> Download
+      </a>
+      <!-- end download link -->
+
+      <!-- counter slideActive -->
+      <button
+        @click="slideActive = !slideActive"
+        class="absolute w-fit h-fit bottom-1.5 left-1.5 py-2 px-3 bg-transparent backdrop-blur-2xl text-[12px] text-zinc-100 rounded-md"
+      >
+        <i class="bi bi-card-text"></i>
+      </button>
+      <!-- counter slideActive -->
+    </figure>
+
+    <figcaption
+      class="absolute h-full top-0 left-0 transition-all duration-300 overflow-hidden"
+      :class="slideActive ? 'w-full' : 'w-0'"
     >
-      <i class="bi bi-arrow-down"></i> Download
-    </a>
+      <div
+        class="@wrap h-full w-2/3 min-w-[150px] p-3 rounded-xl bg-transparent backdrop-blur-2xl"
+        :class="props.textwhite ? 'text-zinc-100' : 'text-zinc-900'"
+      >
+        <div class="@wrap flex justify-between">
+          <h1 class="text-md font-semibold">{{ props.title }}</h1>
+
+          <!-- counter slideActive -->
+          <button @click="slideActive = !slideActive" class="text-md">
+            <i class="bi bi-x"></i>
+          </button>
+          <!-- end counter slideActive -->
+        </div>
+        <p class="text-sm">{{ props.description }}</p>
+      </div>
+    </figcaption>
   </div>
 </template>
 <!-- end template -->

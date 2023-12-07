42a058008ccc6f959d55c30f467da98f7fe488ab
ka-shifuka <aryadewanggapuja@gmail.com>
2023-12-07 20:45:09 +0700

    Add new Structure html and logo

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,37 @@
 <!-- script -->
 <script setup>
   import Images from "./components/Images.vue";
+  import data from "./assets/data.json";
 </script>
 <!-- end script -->
 
 <!-- template -->
 <template>
-  <!-- height ground -->
-  <div
-    class="w-[200vh] -top-20 -left-20 h-20 absolute bg-black blur-xl -z-50"
-  ></div>
-  <!-- end height ground -->
-
   <section class="@Container min-h-screen flex flex-col justify-between">
     <!-- header and main content -->
-    <div class="@wrap p-6 flex flex-col gap-6">
-      header main
-      <Images
-        textwhite
-        title="Ryo yamada"
-        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
-        url="/pictures/images_1.jpeg"
-      ></Images>
-      <Images
-        textwhite
-        title="Ryo yamada"
-        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
-        url="/pictures/images_2.jpeg"
-      ></Images>
-      <Images
-        textwhite
-        title="Ryo yamada"
-        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
-        url="/pictures/images_3.jpeg"
-      ></Images>
-      <Images
-        textwhite
-        title="Ryo yamada"
-        description="dolore tempor exercitation reprehenderit cupidatat nostrud enim mollit ullamco dolore nulla deserunt laborum veniam reprehenderit et nostrud pariatur sint velit"
-        url="/pictures/images_4.jpeg"
-      ></Images>
+    <div class="@wrap p-6">
+      <header class="flex items-center gap-2">
+        <!-- title and icon -->
+        <img class="w-10 h-10 rounded-lg" src="/icon.jpg" />
+        <div class="@wrap flex flex-col justify-center">
+          <h1 class="relative top-1.5 font-semibold text-lg">The Galery</h1>
+          <p class="text-[10px]">Create By Arya Dewangga Puja | ka-shifuka</p>
+        </div>
+        <!-- title and icon -->
+      </header>
+
+      <main class="mt-6 md:flex md:justify-evenly md:flex-wrap">
+        <div class="flex flex-col gap-6 sm:w-[400px]">
+          <div v-for="image in data.i1">
+            <images :url="image.url"></images>
+          </div>
+        </div>
+        <div class="flex flex-col gap-6 sm:w-[400px]">
+          <div v-for="image in data.i2">
+            <images :url="image.url"></images>
+          </div>
+        </div>
+      </main>
     </div>
     <!-- endheader and main content -->
 

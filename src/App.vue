023e5412b95475b5cd1b5bcc9a39558aad25bf9c
ka-shifuka <aryadewanggapuja@gmail.com>
2023-12-09 19:55:04 +0700

    Fix bug

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,33 @@
 
 <!-- template -->
 <template>
-  <section
-    class="@Container flex flex-col justify-between"
-    :class="isload ? 'h-screen' : 'h-fit'"
-  >
+  <section class="@Container flex flex-col justify-between">
     <!-- header and main content -->
     <div class="@wrap p-6">
-      <header class="flex items-center gap-2 select-none">
+      <header class="flex items-center justify-between select-none">
         <!-- title and icon -->
-        <div
-          class="w-10 h-10 rounded-lg bg-cover"
-          style="background-image: url(&quot;/icon.jpg&quot;)"
-        ></div>
-        <div class="@wrap flex flex-col justify-center">
-          <h1 class="relative top-1.5 font-semibold text-lg">The Galery</h1>
-          <p class="text-[10px]">Create By Arya Dewangga Puja | ka-shifuka</p>
+        <div class="@wrap flex items-center gap-2">
+          <div
+            class="w-10 h-10 rounded-lg bg-cover"
+            style="background-image: url(&quot;/icon.jpg&quot;)"
+          ></div>
+          <div class="@wrap flex flex-col justify-center">
+            <h1 class="h-6 font-semibold text-lg">The Galery</h1>
+            <p class="text-[10px]">Create By Arya Dewangga Puja | ka-shifuka</p>
+          </div>
+          <!-- title and icon -->
         </div>
-        <!-- title and icon -->
+
+        <div class="@wrap w-10 h-10 flex items-center justify-end">
+          <!-- github link -->
+          <a
+            class="text-2xl cursor-pointer"
+            href="https://github.com/ka-shifuka/the-galery"
+          >
+            <i class="bi bi-github"></i>
+          </a>
+          <!-- github link -->
+        </div>
       </header>
 
       <main
